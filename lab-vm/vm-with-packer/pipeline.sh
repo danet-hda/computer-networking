9dd07bfb789a5084ad272eea9815a9c118784f65
Martin Stiemerling <mlsgit@users.noreply.github.com>
2023-11-27 10:32:03 +0100

    Revert "23 24 wintersemester 2 lab (#8)" (#9)
    This reverts commit 2ea16aa28fae7e16014585a194f31f3d93f6e075.

--- lab-vm/vm-with-packer/pipeline.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e # abort if there is an issue with any build
-packer init .
-packer build -only='virtualbox-iso.baseimage' .
-packer build -only='virtualbox-ovf.rechnernetze' .
-rm -r output-baseimage
